99670d1af4ec087b5e37fee11eb2208f2089b3d4
NickCraft9288 <112868286+NickCraft9288@users.noreply.github.com>
2023-02-13 13:26:14 +0100

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,53 @@
+<html>
+    <link rel="stylesjeet" href="style.css">
+<?php
+
+$a[0] = "rob";
+$a[1] = "jan";
+$a = array("rob", "jan", "piet");
+
+echo $a[1];
+
+
+
+echo '<table border="1" width="200"  >';
+
+foreach($a as $value){
+    echo "<tr><td>";
+    echo "$value <br>";
+    echo "</td> </tr>";
+}
+
+echo "</table>";
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+
+
+
+
+
+echo "<br>";
+/*var_dump($_POST);*/
+/*echo "<p>hello world</p>";*/
+/*print username*/
+/*echo $_POST["fname"];*/
+echo "<br>";
+
+echo "<br>";
+/*echo $_POST["pwd"];
+*/
+echo '<table border="1" width="200"  >';
+
+foreach($_POST as $_POST){
+    echo "<tr><td>";
+    echo "$_POST     <br>";
+    echo "</td> </tr>";
+}
+
+
+?>
+</html>
